3f0d72fbbd45225f3cf87b39d391e1d1c77e66f1
jia-shan <shan.jennifer.jx@gmail.com>
2026-03-17 14:08:27 +0100

    build+fix: Mistakes in tracking_id and fix env

--- supervision/tracker/byte_tracker/single_object_track.py
+++ supervision/tracker/byte_tracker/single_object_track.py
@@ -83,12 +83,13 @@ class STrack:
 
         self.tracklet_len = 0
         self.state = TrackState.Tracked
-        if frame_id == 1:
-            self.is_activated = True
+        # if frame_id == 1:
+        #     self.is_activated = True
 
-        if self.minimum_consecutive_frames == 1:
-            self.external_track_id = self.external_id_counter.new_id()
-
+        # if self.minimum_consecutive_frames == 1:
+        # self.external_track_id = self.external_id_counter.new_id()
+        self.is_activated = True
+        self.external_track_id = self.external_id_counter.new_id()
         self.frame_id = frame_id
         self.start_frame = frame_id
 
@@ -118,7 +119,7 @@ class STrack:
             self.mean, self.covariance, self.tlwh_to_xyah(new_tlwh)
         )
         self.state = TrackState.Tracked
-        if self.tracklet_len == self.minimum_consecutive_frames:
+        if self.tracklet_len >= self.minimum_consecutive_frames:
             self.is_activated = True
             if self.external_track_id == self.external_id_counter.NO_ID:
                 self.external_track_id = self.external_id_counter.new_id()
